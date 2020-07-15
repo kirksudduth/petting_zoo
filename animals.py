ad70a12b6d680d0cd14e4f3086f8ada32798ab91
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-15 13:26:40 -0500

    modified some stuff and got all my critter objects

---
+++ animals.py
@@ -0,0 +1,158 @@
+from datetime import date
+
+# WALKING CLASSES
+class Llama:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+leif = Llama("Leif", "curly-haired llama")
+print(leif)
+
+class Donkey:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+desmond = Donkey("Desmond", "agreeable donkey")
+print(desmond)
+
+class Sheep:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+shepherd = Sheep("Shepherd", "responsible sheep")
+print(shepherd)
+
+class Ibex:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+ingrid = Ibex("Ingrid", "scared-of-heights ibex")
+print(ingrid)
+
+class Goat:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+geoffrey = Goat("Geoffrey", "billy goat")
+print(geoffrey)
+#END WALKING CLASSES
+
+#START SLITHERING CLASSES
+class Coral_snake:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+carol = Coral_snake("Carol", "deadly")
+print(carol)
+
+class Boa_constrictor:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+bill = Boa_constrictor("Bill", "extra clingy")
+print(bill)
+
+class Rattlesnake:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+ridley = Rattlesnake("Ridley", "rhythmic rattler")
+print(ridley)
+
+class King_cobra:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+keira = King_cobra("Keira", "ruler cobra")
+print(keira)
+
+class Cyclops_snake:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+seth = Cyclops_snake("Seth", "one-eyed snake")
+print(seth)
+#END SLITHERING CLASSES
+
+#START SWIMMING CLASSES
+class Mallard:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+maureen = Mallard("Maureen", "purple mallard")
+print(maureen)
+
+class Beaver:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+bentley = Beaver("Bentley", "cabin building beaver")
+print(bentley)
+
+class Catfish:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+cheryl = Catfish("Cheryl", "talking catfish")
+print(cheryl)
+
+class Platypus:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+prose = Platypus("Prose", "poetic platypus")
+print(prose)
+
+class Nemo:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+noelle = Nemo("Noelle", "navigational fish")
+print(noelle)
+
+#END SWIMMING CLASSES
