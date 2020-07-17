9b661fa05b94768e8973de7ec7298669d0f1c321
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-16 22:33:08 -0500

    hopefully making the right decisions

---
+++ attractions.py
@@ -0,0 +1,28 @@
+class Petting_zoo:
+
+    def __init__(self, name):
+        self.attraction_name = name
+        self.description = "all the animals you could possibly want to pet"
+        self.animals = list()
+
+    def Add_animal(self, animal):
+        self.animals.append(animal)
+
+class Snake_pit:
+    def __init__(self, name):
+        self.attraction_name = name
+        self.description = "legs are overrated, come hang out with these smiling slitherers"
+        self.animals = list()
+    
+    def Add_animal(self, animal):
+        self.animals.append(animal)
+
+class Wetlands:
+    def __init__(self, name):
+        self.attraction_name = name
+        self.description = "gotta love the water -- these critters do"
+        self.animals = list()
+    
+    def Add_animal(self, animal):
+        self.animals.append(animal)
+        
