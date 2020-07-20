b9c771187de822ba5d8089f3ead4e39397a0c6dc
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-20 10:37:46 -0500

    get inheriting

--- animals/animals.py
+++ animals/animals.py
@@ -1,15 +1,39 @@
 from datetime import date
 
-# WALKING CLASSES
-class Llama:
-    def __init__(self, name, species, shift, food, chip_num):
+class Animal:
+    def __init__(self, name, species, food, chip_num):
         self.name = name
         self.species = species
         self.date_added = date.today()
-        self.walking = True
-        self.shift = shift
         self.food = food
         self.__chip_number = chip_num
+
+    @property
+    def chip_number(self):
+        try:
+            return self.__chip_number
+        except AttributeError:
+            return 0
+    
+    @chip_number.setter
+    def chip_number(self, new_chip_number):
+        if type(new_chip_number) is int:
+            self.__chip_number = new_chip_number
+        else:
+            raise TypeError("Please provide a whole integer.")
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f'{self.name} is a {self.species}.'
+
+# WALKING CLASSES
+class Llama:
+    def __init__(self, name, species, shift, food, chip_num):
+        super().__init__():
+        self.walking = True
+        self.shift = shift
 
     @property
     def chip_number(self):
